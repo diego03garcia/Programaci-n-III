1078bb064f135108138d3f92be532dbc67557293
Diego03 <dgarciar30@miumg.edu.gt>
2023-02-16 16:53:13 -0600

    lista simple codigo
    aqui esta el paquete que contiene todo el codigo de lista simple

---
+++ programacion3/src/lista/Main.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista;
+
+
+public class Main {
+    
+    public static void main(String[] args)throws Exception{
+        
+        Listas lista = new Listas();
+        System.out.println("<<-- Ejemplos de lista simple -->>\n");
+        
+        lista.agregarAlFinal(12);
+        lista.agregarAlFinal(15);
+        lista.agregarAlFinal(9);
+        
+        lista.agregarAlInicio(41);
+        lista.agregarAlInicio(6);
+        System.out.println("<<-- Lista -->>");
+        
+        lista.listar();
+        System.out.println("\n\n<<-- Tamaño -->");
+        System.out.println(lista.getTamanio());
+        System.out.println("\n<<-- Obtener el valor del nodo en la posicion 3 -->>");
+        System.out.println(lista.getValor(3));
+        System.out.println("\nInsrta un nodo con valor 16 despues del 15");
+        lista.insertarPorReferencia(15, 16);
+        
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\n\nInsrta un nodo con valor 44 en la posición 3");
+        lista.insertarPorPosicion(3, 44);
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\nActualiza el valor 12 del tercer nodo por 13");
+        lista.editarPorReferencia(12, 13);
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\nActualiza el valor nodo en la posición 0 por 17");
+        lista.editarPorPosicion(0, 17);
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\nElimina el nodo con el valor 41");
+        lista.removerPorReferencia(41); 
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\nElimina el nodo en la posición 4");
+        lista.removerPorPosicion(4); 
+        lista.listar();
+        System.out.print(" | Tamaño: ");
+        System.out.println(lista.getTamanio());
+        System.out.println("\nConsulta si existe el valor 30");
+        System.out.println(lista.buscar(30));
+        System.out.println("\nConsulta la posicion del valor 9");
+        System.out.println(lista.getPosicion(9));
+        System.out.println("\nElimina la lista");
+        lista.eliminar();
+        System.out.println("\nConsulta si la lista está vacia");
+        System.out.println(lista.esVacia());
+        System.out.println("\n\n<<-- Fin de ejemplo lista simple -->>");
+
+
+    }
+    
+    
+}
